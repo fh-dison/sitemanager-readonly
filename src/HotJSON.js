1d11c78046f9a7d2b35a07578a3bc52851937c7e
David Ison <dison@fischerhomes.com>
2019-04-30 13:01:41 -0400

    Live data from server functional.

--- src/HotJSON.js
+++ src/HotJSON.js
@@ -3,7 +3,6 @@ import { HotTable } from '@handsontable/react';
 import axios from 'axios';
 
 // React 16.8 Functional component with hooks
-
 function HotJSON() {
 
   // json could be any kind of data from the server
@@ -11,7 +10,7 @@ function HotJSON() {
 
   useEffect(() => {
 
-    const payload = {
+    const instructions = {
       token: "yJK-1kzbodLxjGQD_8rDMQ",
       data: {
         name: "nameFirst",
@@ -24,26 +23,23 @@ function HotJSON() {
     axios({
       method: "post",
       url: "https://app.fakejson.com/q",
-      data: payload
+      data: instructions,
+
     }).then(function(resp) {
-      console.log (resp);
-      // Do something with fake data
-      const titles = ['Email', 'Name', 'Phone'];
+      // Change individual objects into arrays for handsontable
       const values = resp.data.map(v => {
         return [v.email, v.name, v.phone];
       });
-
-      setJSON([titles, ...values]);
+      setJSON([['Email', 'Name', 'Phone'], ...values]);
 
     });
 
-    console.log ("URL fetch here..")
   }, []);
 
 
   return (
     <React.Fragment>
-      HotJSON
+      HotJSON with server data
       <HotTable data={json} colHeaders={true} rowHeaders={true} width="600" height="300" licenseKey={'non-commercial-and-evaluation'}/>
     </React.Fragment>
 
@@ -51,12 +47,3 @@ function HotJSON() {
 }
 
 export default HotJSON;
-
-
-/*
-    ['', 'Tesla', 'Mercedes', 'Toyota', 'Volvo'],
-      ['2019', 10, 11, 12, 13],
-      ['2020', 20, 11, 14, 13],
-      ['2021', 30, 15, 12, 13]
-
- */
