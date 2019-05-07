2a384e3b5b7984af0fde36a575d70cda38aa7ae0
David Ison <dison@fischerhomes.com>
2019-05-07 14:35:14 -0400

    Minor changes to DataTools

--- src/DataTools.js
+++ src/DataTools.js
@@ -4,30 +4,30 @@
 //  Can use nested try/catch etc for advanced error handling
 //  Ex:  API data can't be parsed properly
 
-  export const dataParser = (data, headers) => {
-    try {
-      return JSON.parse(data);
+export const dataParser = (data, headers) => {
+  try {
+    return JSON.parse(data);
 
-    } catch (e) {
-      // SyntaxError
-      if (e instanceof SyntaxError) {
-        // Was a JSON.parse error
-      }
+  } catch (e) {
+    // SyntaxError
+    if (e instanceof SyntaxError) {
+      // Was a JSON.parse error
     }
   }
+}
 
 
-  export const dataFormatter = (data) => {
-    try {
-      const values = data.map(v => {
-        return [v.email, v.name, v.phone];
-      });
-      return [['Email', 'Name', 'Phone'], ...values];
-    } catch (e) {
-      //  Mapping error, some other error
-    }
-
+export const dataFormatter = (data) => {
+  try {
+    const values = data.map(v => {
+      return [v.email, v.name, v.phone];
+    });
+    return [['Email', 'Name', 'Phone'], ...values];
+  } catch (e) {
+    //  Mapping error, some other error
   }
+
+}
 
  
 
